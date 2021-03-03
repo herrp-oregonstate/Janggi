cda9002526f6aea4313c692327959a5efe6c04e3
herrp-oregonstate <herrp@oregonstate.edu>
2021-03-02 19:46:43 -0600

    Added method to convert to and from algebraic notation. Also added method to calculate possible movements for soldiers and guards.

--- JanggiGame.py
+++ JanggiGame.py
@@ -3,62 +3,6 @@
 # Description:
 
 
-class Soldier:
-    """
-
-    """
-
-    pass
-
-
-class Guard(Soldier):
-    """
-
-    """
-
-    pass
-
-
-class General(Guard):
-    """
-
-    """
-
-    pass
-
-
-class Horse:
-    """
-
-    """
-
-    pass
-
-
-class Elephant(Horse):
-    """
-
-    """
-
-    pass
-
-
-class Chariot:
-    """
-
-    """
-
-    pass
-
-
-class Cannon(Chariot):
-    """
-
-    """
-
-    pass
-
-
 class JangiGame:
     """
 
@@ -160,11 +104,296 @@ class JangiGame:
 
         pass
 
-    def algebraic_notation_to_list(self, algebraic_notation):
+    def alphabetic_to_index(self, algebraic_notation):
+        """
+        Converts the alphabetic part of the algebraic notation into its corresponding index.
         """
 
-        :param algebraic_notation:
+        alpha_dict = {
+            "a": 0, "b": 1, "c": 2,
+            "d": 3, "e": 4, "f": 5,
+            "g": 6, "h": 7, "i": 8
+        }
+
+        return alpha_dict.get(algebraic_notation[0])
+
+    def numeric_to_index(self, algebraic_notation):
+        """
+        Converts the numeric part of the algebraic notation to its corresponding index.
+        """
+
+        num_dict = {
+            "1": 0, "2": 1, "3": 2,
+            "4": 3, "5": 4, "6": 5,
+            "7": 6, "8": 7, "9": 8, "10": 9
+        }
+
+        return num_dict.get(algebraic_notation[1])
+
+    def index_to_alphabetic(self, index):
+        """
+        Converts the index into the alphabetic part of the algebraic notation.
+        """
+
+        alpha_dict = {
+            0: "a", 1: "b", 2: "c",
+            3: "d", 4: "e", 5: "f",
+            6: "g", 7: "h", 8: "i"
+        }
+
+        return alpha_dict.get(index)
+
+    def index_to_numeric(self, index):
+        """
+        Converts the index into the numeric part of the algebraic notation
+        """
+
+        num_dict = {
+            0: "1", 1: "2", 2: "3",
+            3: "4", 4: "5", 5: "6",
+            6: "7", 7: "8", 8: "9", 9: "10"
+        }
+
+        return num_dict.get(index)
+
+    def algebraic_notation_convertor(self, algebraic_notation):
+        """
+        Converts algebraic notation to index notation. Returns a tuple containing the two values.
+        """
+
+        return [self.alphabetic_to_index(algebraic_notation), self.numeric_to_index(algebraic_notation)]
+
+    def indices_to_algebraic_notation(self, x_index, y_index):
+        """
+        Converts the indices to algebraic notation.
+        """
+
+        return self.index_to_alphabetic(x_index) + self.index_to_numeric(y_index)
+
+    def valid_moves(self, piece):
+        """
+
+        :param piece:
         :return:
         """
 
         pass
+
+
+class JangiPiece(JangiGame):
+    """
+    A Jangi piece belonging a player and it has position on the board.
+    """
+
+    def __init__(self, player, position):
+        """
+        Creates a Jangi piece belonging to a player's color and initialize its position on the board.
+        """
+
+        self._player = player
+        self._position = position
+
+    def get_player(self):
+        """
+        Return the player who owns this piece.
+        """
+
+        return self._player
+
+    def get_position(self):
+        """
+        Returns the position of the piece.
+        """
+
+        return self._position
+
+    def set_position(self, position):
+        """
+        Sets the position of the piece to a new location.
+        """
+
+        self._position = position
+
+
+class Soldier(JangiPiece):
+    """
+    A soldier piece that can move forward one space or move to the left or right one space. It cannot go backwards.
+    """
+
+    def __init__(self, player, position):
+        """
+        Initializes a soldier piece to belong to a player and sets its position on the board.
+        """
+
+        super().__init__(player, position)
+
+    def possible_movements(self):
+        """
+        Returns a set of possible movements for the soldier piece.
+        """
+
+        possible_movements_set = set()
+        x_axis = self.alphabetic_to_index(self.get_position())
+        y_axis = self.numeric_to_index(self.get_position())
+
+        # Both players are able to move left or right, unless they are at the edge of the board.
+        if x_axis != 0 and x_axis != 8:
+            possible_movements_set.add(self.indices_to_algebraic_notation(x_axis + 1, y_axis))
+            possible_movements_set.add(self.indices_to_algebraic_notation(x_axis - 1, y_axis))
+        elif x_axis == 0:
+            possible_movements_set.add(self.indices_to_algebraic_notation(x_axis + 1, y_axis))
+        else:
+            possible_movements_set.add(self.indices_to_algebraic_notation(x_axis - 1, y_axis))
+
+        # Red can only move to a higher index (down the board), while blue can only move to a lower index (up the board)
+        if self.get_player() == "red":
+            if y_axis != 9:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis + 1))
+        else:
+            if y_axis != 0:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis - 1))
+
+        return possible_movements_set
+
+    pass
+
+
+class Guard(JangiPiece):
+    """
+    A guard piece that can move forward one space, backwards one space, or left or right one space. It is only able to
+    move within the palace.
+    """
+
+    def __init__(self, player, position):
+        """
+        Initializes a guard piece to belong to a player and sets its position on the board.:
+        """
+
+        super().__init__(player, position)
+
+    def possible_movements(self):
+        """
+        Returns a set of possible movements for the soldier piece.
+        """
+        possible_movements_set = set()
+        x_axis = self.alphabetic_to_index(self.get_position())
+        y_axis = self.numeric_to_index(self.get_position())
+
+        if self.get_player() == "red":
+
+            # Allows horizontal movement within the red palace only.
+            if x_axis != 3 and x_axis != 5:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis + 1, y_axis))
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis - 1, y_axis))
+            elif x_axis == 3:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis + 1, y_axis))
+            elif x_axis == 5:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis - 1, y_axis))
+
+            # Allows vertical movement within the red palace only.
+            if y_axis != 0 or y_axis != 2:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis + 1))
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis - 1))
+            elif y_axis == 0:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis + 1))
+            elif y_axis == 2:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis - 1))
+
+        else:
+
+            # Allows horizontal movement within the blue palace only.
+            if x_axis != 3 and x_axis != 5:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis + 1, y_axis))
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis - 1, y_axis))
+            elif x_axis == 3:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis + 1, y_axis))
+            elif x_axis == 5:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis - 1, y_axis))
+
+            # Allows vertical movement within the blue palace only.
+            if y_axis != 7 or y_axis != 9:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis + 1))
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis - 1))
+            elif y_axis == 9:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis + 1))
+            elif y_axis == 7:
+                possible_movements_set.add(self.indices_to_algebraic_notation(x_axis, y_axis - 1))
+
+    pass
+
+
+class General(Guard):
+    """
+    A general piece that can move forward one space, backwards one space, or left or right one space. It is only able to
+    move within the palace. The player loses if his general is checkmated.
+    """
+
+    def __init__(self, player, position):
+        """
+        Initializes a general piece to belong to a player and sets its position on the board.
+        """
+
+        super().__init__(player, position)
+
+    pass
+
+
+class Horse:
+    """
+
+    """
+
+    def __init__(self, player, position):
+        """
+        Initializes a horse piece to belong to a player and sets its position on the board.
+        """
+
+        super().__init__(player, position)
+
+    pass
+
+
+class Elephant(Horse):
+    """
+
+    """
+
+    def __init__(self, player, position):
+        """
+        Initializes an elephant piece to belong to a player and sets its position on the board.
+:
+        """
+
+        super().__init__(player, position)
+
+    pass
+
+
+class Chariot:
+    """
+
+    """
+
+    def __init__(self, player, position):
+        """
+        Initializes a chariot piece to belong to a player and sets its position on the board.
+        """
+
+        super().__init__(player, position)
+
+    pass
+
+
+class Cannon(Chariot):
+    """
+
+    """
+
+    def __init__(self, player, position):
+        """
+        Initializes a cannon piece to belong to a player and sets its position on the board.
+        """
+
+        super().__init__(player, position)
+
+    pass
